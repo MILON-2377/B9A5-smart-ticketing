ee3f7962fa24e7dd8ee39d36535dad6d92c641f0
MILON-2377 <153436745+MILON-2377@users.noreply.github.com>
2024-02-18 19:21:01 +0800

    make the buttons dynamic and updated the left seat

--- script/ticketing.js
+++ script/ticketing.js
@@ -6,5 +6,31 @@ document.addEventListener("DOMContentLoaded", () => {
     buyTicketButton.addEventListener("click", () => {
         targetSection.scrollIntoView("{ behavior: 'smooth' }");
     })
+   
 })
+
+function updatingLeftSeat(idName){
+    const avaibleSeatNumber = document.getElementById(idName);
+    const seatNumber = avaibleSeatNumber.innerText;
+    const avaibleNumber = parseInt(seatNumber);
+
+    const leftSeatNumber = avaibleNumber - 1;
+    avaibleSeatNumber.innerText = leftSeatNumber;
+}
+
+
+
+function updateSeatNumber(value){
+
+}
+
+const seatContainBox = document.querySelectorAll("#seats-contain-box");
+for(const item of seatContainBox){
+    item.addEventListener("click", (e) => {
+        if(e.target.classList[0] === "btn"){
+            e.target.classList.add("bg-[#1DD100]");
+            updatingLeftSeat("seats-left-box");
+        }
+    })
+}
 
